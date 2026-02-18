56d1a5473779beb5d1ca94213be834745f0695b7
Franka <floarescou@yandex.ru>
2026-02-18 21:36:28 +0300

    scroll into view js for navigation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-// alert("Hello, JavaScript");
+// плавная прокрутка к нужной секции
+// пункты меню
+const productsBtn = document.getElementById("productsBtn");
+const aboutBtn = document.getElementById("aboutBtn");
+const orderBtn = document.getElementById("orderBtn");
+
+// секции страницы
+const aboutSection = document.getElementById("aboutSection");
+const productsSection = document.getElementById("productsSection");
+const orderSection = document.getElementById("orderSection");
+
+productsBtn.addEventListener('click', function (e) {
+    productsSection.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+});
+aboutBtn.addEventListener('click', function (e) {
+    aboutSection.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+});
+orderBtn.addEventListener('click', function (e) {
+    orderSection.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+});
+
+// .. плавная прокрутка к нужной секции
